94ea4e263ceb3e9b138ff1cb54d2e531c9168817
justindegaetano <105392908+justindegaetano@users.noreply.github.com>
2024-09-19 12:57:37 -0400

    Initialize React, Tailwind & Shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
-export default {
+export default defineConfig({
   output: 'server',
-};
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+});
