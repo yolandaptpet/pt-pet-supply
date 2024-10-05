97e298b9e3b4405f3905aeba1b60e3e6005a6da4
justindegaetano <105392908+justindegaetano@users.noreply.github.com>
2024-10-05 02:18:50 -0400

    Responsive styling & initialize mobile sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import alpinejs from '@astrojs/alpinejs';
+
 import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
+
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [
     react(),
+    alpinejs(),
     tailwind({
       applyBaseStyles: false,
     }),
